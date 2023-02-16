33ecb96e745e8c6e6a5a886c23d2d65011222015
Yongjian.wu <49546440+Wu-Yongjian@users.noreply.github.com>
2023-02-16 10:02:16 +0800

    new java
    1

---
+++ hello.java
@@ -0,0 +1,21 @@
+//public class HelloWorld：
+//HelloWorld是一个public的公有的类
+ 
+public class HelloWorld {
+ 
+    //HelloWorld {}：
+    //花括号{}：表示了HelloWorld这个类的开始和结束
+ 
+    //public static void main(String[] args)：
+    //主方法，程序的入口
+    //main(){}:方法的开始和结束
+ 
+    public static void main(String[] args) {
+ 
+        //System.out.println("HelloWorld");:
+        //输出HelloWorld到屏幕
+        //;代表程序结束
+        
+        System.out.println("HelloWorld");
+    }
+}
